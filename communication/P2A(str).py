4662d1d4f2a265319061a42f942e327d8bd51562
Jason Juhyun Kim <mtinet@hanmail.net>
2019-12-09 20:47:35 +0900

    Update P2A(str).py

--- communication/P2A(str).py
+++ communication/P2A(str).py
@@ -5,7 +5,7 @@ arduino = serial.Serial('/dev/cu.usbmodem141201', 9600)
 while(1):
     c = input()
     print(type(c))          # 유니코드로 입력 받는다.
-    c= c.encode('utf-8')    # 유니코드 문자열을 바이트 문자열로 바꾼다.
+    c= (c+'\n').encode('utf-8')    # 유니코드 문자열을 바이트 문자열로 바꾼다.
     arduino.write(c)
     print(type(c))        # 시리얼 쓰기는 바이트만 가능하다.
     # sum = arduino.readline()
